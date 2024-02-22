e7277b79d85573dccfcd4b9ecfc61a21f70eedfb
Arya <arya.nooralizadeh@gmail.com>
2024-02-22 18:04:08 +1100

    modified cout text to include my name

---
+++ build/CMakeFiles/convertor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/convertor.dir/convertor.cpp.o"
+  "CMakeFiles/convertor.dir/convertor.cpp.o.d"
+  "convertor"
+  "convertor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/convertor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
